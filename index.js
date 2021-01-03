6867e275a8248bc9b72d9cfad31201130bd69b7c
Christina Brenia <christinabrenia@Christinas-MBP-34.home>
2021-01-03 16:57:17 -0500

    added correction to prompts

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
 
+// function write() {
+
+//     startHtml() 
+//     console.log("Hi")
+    
+
+// }
+
 inquirer.prompt ([{
     //Team Manager
         message: "Enter the name of your teammember:",
@@ -40,7 +48,27 @@ name: "id"
     message: "Enter your team manager's office number:",
     name: "officeNumber"
     
-}])
+}]).then(function({name,role,id,email}){
+    let roleInfo = '';
+    if(role === "Engineer") {
+        roleInfo = "GitHub Username"
+    } else if (role === "Intern") {
+        roleInfo = "school name"
+    } else if (role === "Manager"){
+        roleInfo = "office Phone Number"
+    }
+    inquirer.prompt([{
+        type:"list",
+        message:"Would you like to add another team member?",
+        choices: [
+            "yes",
+            "no"
+        ],
+        name: "addMoreMembers"
+    }])
+    
+})
 
+// write()
 
 
