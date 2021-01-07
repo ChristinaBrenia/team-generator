7c63f4e79d4e6b42db91342b85a4fcaabda02a6c
Christina Brenia <christinabrenia@Christinas-MBP-23.home>
2021-01-06 21:32:21 -0500

    added object for manager

--- index.js
+++ index.js
@@ -1,74 +1,49 @@
-const inquirer = require('inquirer')
-const fs = require('fs')
+//inquirer prompts user with questions
+const inquirer = require("inquirer");
+const fs = require("fs");
+const Manager = require("./lib/manager")
 
-// function write() {
 
-//     startHtml() 
-//     console.log("Hi")
-    
+//jest runs tests
+const jest = require("jest");
 
-// }
+finalTeamArray = []
 
+function promptUser() {
 inquirer.prompt ([{
     //Team Manager
+        type: "input",
         message: "Enter the name of your teammember:",
         name: "name"
 },
-
 {
-    type:'list',
-
-
-    message: "Select their role:",
-    choices: [
-        "Engineer",
-        "Intern",
-        "Manager"
-    ],
-    name: "role"
-    
-    },
-
+    type: "input",
+    name:"id",
+    message: "Enter ID of manager:"
+},
 {
-
-message: "Enter your team manager's employee ID:",
-name: "id"
+    type: "input",
+    name:"email",
+    message: "Enter the manager's email",
+},
+{
+    type:"Input",
+    name: 'phone',
+    message:"Enter the manager's phone number:"
 
 },
-
-{
-
-    message: "Enter your team manager's email address:",
-    name: "email"
-    
-},
-
-{
-
-    message: "Enter your team manager's office number:",
-    name: "officeNumber"
-    
-}]).then(function({name,role,id,email}){
-    let roleInfo = '';
-    if(role === "Engineer") {
-        roleInfo = "GitHub Username"
-    } else if (role === "Intern") {
-        roleInfo = "school name"
-    } else if (role === "Manager"){
-        roleInfo = "office Phone Number"
-    }
-    inquirer.prompt([{
-        type:"list",
-        message:"Would you like to add another team member?",
-        choices: [
-            "yes",
-            "no"
-        ],
-        name: "addMoreMembers"
-    }])
-    
+]).then(function (data) {
+    const name = data.name
+    const id = data.id
+    const email = data.email
+    const phone = data.phone
+    const teamMember = new Manager (name, id, email, phone)
+    finalTeamArray.push(teamMember)
+    console.log(finalTeamArray)
 })
+}
 
-// write()
+
+promptUser()
 
 
